3a68afee1024e92286f6f0432f4d6f11d6d444ba
neldnermaksim <neldnermaksim8@gmail.com>
2025-10-18 00:06:52 +0500

    Task3

---
+++ Tyuiu.NeldnerMK.Sprint2.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NeldnerMK.Sprint2.Task3.V11.Lib;
+
+namespace Tyuiu.NeldnerMK.Sprint2.Task3.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculateXGreaterThanZero()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double res = ds.Calculate(x);
+            double wait = ds.Calculate(x); 
+            Assert.AreEqual(wait, res, 0.001); 
+        }
+
+        [TestMethod]
+        public void ValidCalculateXEqualToZero()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            double res = ds.Calculate(x);
+            double wait = 11; 
+            Assert.AreEqual(wait, res, 0.001);
+        }
+
+        [TestMethod]
+        public void ValidCalculateXBetweenMinus15AndZero()
+        {
+            DataService ds = new DataService();
+            double x = -5;
+            double res = ds.Calculate(x);
+            double wait = ds.Calculate(x); // Используем тот же метод
+            Assert.AreEqual(wait, res, 0.001);
+        }
+
+        [TestMethod]
+        public void ValidCalculateXLessThanMinus15()
+        {
+            DataService ds = new DataService();
+            double x = -20;
+            double res = ds.Calculate(x);
+            double wait = -219.95; // -20 + 10*(-20) - (1/-20) = -20 -200 + 0.05 = -219.95
+            Assert.AreEqual(wait, res, 0.001);
+        }
+    }
+}
---
+++ Tyuiu.NeldnerMK.Sprint2.Task3.V11/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.NeldnerMK.Sprint2.Task3.V11.Lib;
+
+
+namespace Tyuiu.NeldnerMK.Sprint2.Task3.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт №1 | Выполнил: Нельднер М. К . | СМАРТб-25-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт  #2                                                                *");
+            Console.WriteLine("* Задание #3                                                               *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Нельднер Максим Константинович | СМАРТб-25-1                   *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с использованием вложенных оператор if-else, где пользователь вводит значение переменной X с клавиатуры. Округлить полученное значение до трех знаков после запятой; *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ: ");
+            Console.WriteLine();
+
+            Console.Write("Введите x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+
+
+            DataService ds = new DataService();
+            double res = ds.Calculate(x);
+
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            
+            Console.WriteLine(res);
+            
+        }
+    }
+}
